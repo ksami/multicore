7b2e6c53b4144f883d0dcdded7053a6e2f3f87bf
ksami <ksami.ihide@gmail.com>
2015-03-20 03:01:28 +0900

    complete experiment fuse multiply-add

---
+++ fma/test.cpp
@@ -0,0 +1,24 @@
+#include <emmintrin.h>
+
+#define N 1000
+
+int main(void) {
+	__m64 a;
+	__m64 b;
+	__m64 c;
+	__m64 res;
+
+	for(int i=0; i<N; i++) {
+		for(int j=0; j<N; j++) {
+			for(int k=0; k<N; k++) {
+				a = _mm_set_pi32(0, i);
+				b = _mm_set_pi32(0, j);
+				c = _mm_set_pi32(0, k);
+				res = _mm_madd_pi16(a, b);
+				//res = _mm_add_pi32(_mm_mul_su32(a, b), c);
+			}
+		}
+	}
+
+	return 0;
+}
