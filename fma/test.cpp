7f7a6275397fd83b1a87800ae53b2a84fcb59b29
ksami <ksami.ihide@gmail.com>
2015-03-24 14:09:25 +0900

    modify for fp

--- fma/test.cpp
+++ fma/test.cpp
@@ -1,26 +1,43 @@
 #include <emmintrin.h>
+#include <iostream>
+#include <limits>
 
-#define N 1000
+#define N 10
+
+using namespace std;
+
+typedef numeric_limits<double> dbl;
 
 // ONLY tested timing with integers
 // TODO: run tests on floats to compare accuracy
 int main(void) {
-	__m64 a;
-	__m64 b;
-	__m64 c;
-	__m64 res;
+	__m128d a;
+	__m128d b;
+	__m128d c;
+	__m128d res;
+	double result;
 
 	for(int i=0; i<N; i++) {
 		for(int j=0; j<N; j++) {
 			for(int k=0; k<N; k++) {
-				a = _mm_set_pi32(0, i);
-				b = _mm_set_pi32(0, j);
-				c = _mm_set_pi32(0, k);
-				res = _mm_madd_pi16(a, b);
-				//res = _mm_add_pi32(_mm_mul_su32(a, b), c);
+				a = _mm_set_sd(9.5);  //sets lower 64 to num, upper to 0.0
+				b = _mm_set_sd(10.0);
+				c = _mm_set_sd(5.0);
+				res = _mm_add_sd(_mm_mul_sd(a, b));
+
+				// a = _mm_set_pi32(0, i);
+				// b = _mm_set_pi32(0, j);
+				// c = _mm_set_pi32(0, k);
+				// res = _mm_madd_pi16(a, b);
+				// //res = _mm_add_pi32(_mm_mul_su32(a, b), c);
 			}
 		}
 	}
+
+	result = _mm_cvtsd_f64(res);
+
+	cout.precision(dbl::digits10);
+	cout << "result is " << fixed << result << endl;
 
 	return 0;
 }
