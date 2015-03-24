cc025a09c0c3fd574597a40a5492bdca792d1a67
ksami <ksami.ihide@gmail.com>
2015-03-24 20:34:32 +0900

    change

--- fma/test.cpp
+++ fma/test.cpp
@@ -3,7 +3,7 @@
 #include <limits>
 #include <x86intrin.h>
 
-#define N 2000000000
+#define N 100
 #define A 100.1
 #define B 0.03
 
@@ -11,31 +11,30 @@ using namespace std;
 
 typedef numeric_limits<double> dbl;
 
-//extern __m128d _mm_fmadd_sd(__m128d a, __m128d b, __m128d c);
-
-// ONLY tested timing with integers
-// TODO: run tests on floats to compare accuracy
 int main(void) {
 	__m128d a;
 	__m128d b;
-	__m128d c;
 	__m128d res;
 	double result;
 
-  res = _mm_setzero_pd();  //sets both upper and lower to 0.0
-	
-  for(int i=0; i<N; i++) {
-		a = _mm_set_sd(A);  //sets lower 64 to num, upper to 0.0
-		b = _mm_set_sd(B);
-		c = _mm_set_sd(_mm_cvtsd_f64(res));
-		// res = _mm_add_sd(_mm_mul_sd(a, b), c);
-		res = _mm_fmadd_sd(a, b, c);
+	double arrA[N];
+	double arrB[N];
 
-		// a = _mm_set_pi32(0, i);
-		// b = _mm_set_pi32(0, j);
-		// c = _mm_set_pi32(0, k);
-		// res = _mm_madd_pi16(a, b);
-		// //res = _mm_add_pi32(_mm_mul_su32(a, b), c);
+	//array init
+	for(int i=0; i<N; i++) {
+		arrA[i] = 1e-9;
+		arrB[i] = 3e-7;
+	}
+
+  	res = _mm_setzero_pd();  //sets both upper and lower to 0.0
+	
+  	for(int i=0; i<N; i++) {
+  		a = _mm_set_sd(arrA[i]);  //sets lower 64 to num, upper to 0.0
+  		for(int j=0; j<N; j++) {
+			b = _mm_set_sd(arrB[j]);
+			// res = _mm_add_sd(_mm_mul_sd(a, b), res);
+			res = _mm_fmadd_sd(a, b, res);
+		}
 	}
 
 	result = _mm_cvtsd_f64(res);
