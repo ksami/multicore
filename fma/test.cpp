244da6eb6d1ae8034d829972cd7e13c1062ea1e8
ksami <ksami.ihide@gmail.com>
2015-03-24 16:48:04 +0900

    new results

--- fma/test.cpp
+++ fma/test.cpp
@@ -3,9 +3,9 @@
 #include <limits>
 #include <x86intrin.h>
 
-#define N 10
+#define N 2000000000
 #define A 100.1
-#define B 0.06
+#define B 0.03
 
 using namespace std;
 
@@ -25,21 +25,17 @@ int main(void) {
   res = _mm_setzero_pd();  //sets both upper and lower to 0.0
 	
   for(int i=0; i<N; i++) {
-		for(int j=0; j<N; j++) {
-			for(int k=0; k<N; k++) {
-				a = _mm_set_sd(A);  //sets lower 64 to num, upper to 0.0
-				b = _mm_set_sd(B);
-				c = _mm_set_sd(_mm_cvtsd_f64(res));
-				// res = _mm_add_sd(_mm_mul_sd(a, b), c);
-				res = _mm_fmadd_sd(a, b, c);
+		a = _mm_set_sd(A);  //sets lower 64 to num, upper to 0.0
+		b = _mm_set_sd(B);
+		c = _mm_set_sd(_mm_cvtsd_f64(res));
+		// res = _mm_add_sd(_mm_mul_sd(a, b), c);
+		res = _mm_fmadd_sd(a, b, c);
 
-				// a = _mm_set_pi32(0, i);
-				// b = _mm_set_pi32(0, j);
-				// c = _mm_set_pi32(0, k);
-				// res = _mm_madd_pi16(a, b);
-				// //res = _mm_add_pi32(_mm_mul_su32(a, b), c);
-			}
-		}
+		// a = _mm_set_pi32(0, i);
+		// b = _mm_set_pi32(0, j);
+		// c = _mm_set_pi32(0, k);
+		// res = _mm_madd_pi16(a, b);
+		// //res = _mm_add_pi32(_mm_mul_su32(a, b), c);
 	}
 
 	result = _mm_cvtsd_f64(res);
