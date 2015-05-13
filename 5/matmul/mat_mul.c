fe035ac9a84c0b4241a3e608585d25f10d1ba4c7
ksami <ksami.ihide@gmail.com>
2015-05-13 20:56:22 +0900

    add debug msg

--- 5/matmul/mat_mul.c
+++ 5/matmul/mat_mul.c
@@ -141,6 +141,9 @@ int main(int argc, char** argv)
         hostB[i] = (float) i * 2;
     }
 
+    //debug
+    printf("starting opencl\n");
+
     // OpenCL //
     // Obtain a list of available OpenCL platforms
     cl_platform_id platform;
@@ -153,6 +156,10 @@ int main(int argc, char** argv)
     // Create an OpenCL context on a GPU device
     cl_context context;
     context = clCreateContext(0, 1, &device, NULL, NULL, NULL);
+
+    //debug
+    printf("context created\n");
+
 
     // Create a command queue and attach it to the compute device
     // (in-order queue)
@@ -167,6 +174,9 @@ int main(int argc, char** argv)
     bufferB = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeB, NULL, NULL);
     bufferC = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeC, NULL, NULL);
 
+    //debug
+    printf("buffers allocated\n");
+
 
     // Create an OpenCL program object for the context 
     // and load the kernel source into the program object
@@ -174,13 +184,25 @@ int main(int argc, char** argv)
     size_t kernel_src_len = strlen(kernel_src);
     program = clCreateProgramWithSource(context, 1, (const char**) &kernel_src, &kernel_src_len, NULL);
 
+    //debug
+    printf("program created\n");
+
+
     // Build (compile and link) the program executable 
     // from the source or binary for the device
     clBuildProgram(program, 1, &device, NULL, NULL, NULL);
 
+    //debug
+    printf("program built\n");
+
+
     // Create a kernel object from the program
     cl_kernel kernel;
     kernel = clCreateKernel(program, "vec_add", NULL);
+
+    //debug
+    printf("kernel created\n");
+
 
     // Set the arguments of the kernel
     clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*) &bufferA);
@@ -192,6 +214,10 @@ int main(int argc, char** argv)
     clEnqueueWriteBuffer(command_queue, bufferA, CL_FALSE, 0, sizeA, hostA, 0, NULL, NULL);
     clEnqueueWriteBuffer(command_queue, bufferB, CL_FALSE, 0, sizeB, hostB, 0, NULL, NULL);
 
+    //debug
+    printf("buffers written\n");
+
+
     // The kernel index space is one dimensional
     // Specify the number of total work-items in the index space
     size_t global[1] = { SIZE };
@@ -199,6 +225,10 @@ int main(int argc, char** argv)
     size_t local[1] = { 16 };
     // Execute the kernel
     clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, global, local, 0, NULL, NULL);
+
+    //debug
+    printf("kernel executed\n");
+
 
     // Wait until the kernel command completes 
     // (no need to wait because the command_queue is an in-order queue)
