59637a7bc72a4d113b84f64e4d5b7786702e3287
ksami <ksami.ihide@gmail.com>
2015-05-26 22:51:43 +0900

    fastest

--- 6/kmeans/kmeans_openmp.cpp
+++ 6/kmeans/kmeans_openmp.cpp
@@ -43,27 +43,32 @@ void kmeans(int iteration_n, int class_n, int data_n, Point* centroids, Point* d
                     }
                 }
             }
-        }
-
-        // Update step
-        // Clear sum buffer and class count
-        for (class_i = 0; class_i < class_n; class_i++) {
-            centroids[class_i].x = 0.0;
-            centroids[class_i].y = 0.0;
-            count[class_i] = 0;
-        }
-
-        // Sum up and count data for each class
-        for (data_i = 0; data_i < data_n; data_i++) {         
-            centroids[partitioned[data_i]].x += data[data_i].x;
-            centroids[partitioned[data_i]].y += data[data_i].y;
-            count[partitioned[data_i]]++;
-        }
         
-        // Divide the sum with number of class for mean point
-        for (class_i = 0; class_i < class_n; class_i++) {
-            centroids[class_i].x /= count[class_i];
-            centroids[class_i].y /= count[class_i];
+
+            // Update step
+            // Clear sum buffer and class count
+            for (class_i = 0; class_i < class_n; class_i++) {
+                centroids[class_i].x = 0.0;
+                centroids[class_i].y = 0.0;
+                count[class_i] = 0;
+            }
+
+            // Sum up and count data for each class
+            #pragma omp for
+            for (data_i = 0; data_i < data_n; data_i++) {
+                #pragma omp atomic
+                centroids[partitioned[data_i]].x += data[data_i].x;
+                #pragma omp atomic
+                centroids[partitioned[data_i]].y += data[data_i].y;
+                #pragma omp atomic
+                count[partitioned[data_i]]++;
+            }
+        
+            // Divide the sum with number of class for mean point
+            for (class_i = 0; class_i < class_n; class_i++) {
+                centroids[class_i].x /= count[class_i];
+                centroids[class_i].y /= count[class_i];
+            }
         }
     }
 }
